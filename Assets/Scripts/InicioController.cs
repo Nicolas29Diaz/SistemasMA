a52013a7923b576d050db303c30bf923ba3a7425
Nicolas29Diaz <nicolas_san.diaz@uao.edu.co>
2023-11-19 23:12:10 -0500

    Merge branch 'pr/67' into V3.0.0

--- Assets/Scripts/InicioController.cs
+++ Assets/Scripts/InicioController.cs
@@ -9,70 +9,123 @@ public class InicioController : MonoBehaviour
     public GameObject canvasInicio;
     public GameObject canvasJugador;
     public GameObject canvasHistoria;
+    public GameObject selectPj;
+    public GameObject niño1;
+    public GameObject niño2;
+    public GameObject niña1;
+    public GameObject niña2;
+    public GameObject confirmationSelectPj;
+    private Sprite sprite;
+    public GameObject selectedPj;
+    public GameObject selectedPj2;
+    private Image image;
+    private Image image2;
+    public GameObject presentacion;
+    public GameObject seleccionarPais;
+    public GameObject paisBrazil;
+    public GameObject paisArgentina;
+    public GameObject controls;
 
-    public GameObject botonHombre;
-    public Button hombreBTransform;
 
-    public GameObject botonMujer;
-    public Button mujerBTransform;
+    private void Start()
+    {
+        canvasInicio.SetActive(true);
+        canvasJugador.SetActive(false);
+        canvasHistoria.SetActive(false);
+    }
 
-    public GameObject botonYes;
-    public GameObject botonNo;
-
-    public GameObject panelHistoria1;
-    public GameObject panelHistoria2;
-    public GameObject panelHistoria3;
-
-    public void BotonInicio(){ 
+    public void StartGame()
+    {
         canvasInicio.SetActive(false);
         canvasJugador.SetActive(true);
     }
 
-    public void SeleccionHombre(){
-        botonMujer.SetActive(false);
-        RectTransform botonTransform = hombreBTransform.GetComponent<RectTransform>();
-        botonTransform.anchoredPosition = new Vector3(0,0,0);
-        botonYes.SetActive(true);
-        botonNo.SetActive(true);
+    public void PjSelected(int niño)
+    {
+      
+        if (niño == 0)
+        {
+            sprite = niño1.GetComponent<Image>().sprite;
+            GlobalGameController.instance.SetPersonaje(sprite);
+            image = selectedPj.GetComponent<Image>();
+            image.sprite = GlobalGameController.instance.GetPersonaje();
+            image2 = selectedPj2.GetComponent<Image>();
+            image2.sprite = GlobalGameController.instance.GetPersonaje();
+        }
+        else if (niño ==1){
+            sprite = niño2.GetComponent<Image>().sprite;
+            GlobalGameController.instance.SetPersonaje(sprite);
+            image = selectedPj.GetComponent<Image>();
+            image.sprite = GlobalGameController.instance.GetPersonaje();
+            image2 = selectedPj2.GetComponent<Image>();
+            image2.sprite = GlobalGameController.instance.GetPersonaje();
+        }
+        else if( niño == 2)
+        {
+            sprite = niña1.GetComponent<Image>().sprite;
+            GlobalGameController.instance.SetPersonaje(sprite);
+            image = selectedPj.GetComponent<Image>();
+            image.sprite = GlobalGameController.instance.GetPersonaje();
+            image2 = selectedPj2.GetComponent<Image>();
+            image2.sprite = GlobalGameController.instance.GetPersonaje();
+        }
+        else if (niño == 3)
+        {
+            sprite = niña2.GetComponent<Image>().sprite;
+            GlobalGameController.instance.SetPersonaje(sprite);
+            image = selectedPj.GetComponent<Image>();
+            image.sprite = GlobalGameController.instance.GetPersonaje();
+            image2 = selectedPj2.GetComponent<Image>();
+            image2.sprite = GlobalGameController.instance.GetPersonaje();
+        }
+        selectPj.SetActive(false);
+        confirmationSelectPj.SetActive(true);
     }
 
-    public void SeleccionMujer(){
-        botonHombre.SetActive(false);
-        RectTransform botonTransform = mujerBTransform.GetComponent<RectTransform>();
-        botonTransform.anchoredPosition = new Vector3(0,0,0);
-        botonYes.SetActive(true);
-        botonNo.SetActive(true);
+    public void noSelected()
+    {
+        selectPj.SetActive(true);
+        confirmationSelectPj.SetActive(false);
+        GlobalGameController.instance.SetPersonaje(null);
     }
 
-    public void BotonNo(){
-        botonYes.SetActive(false);
-        botonNo.SetActive(false);
-        
-        botonHombre.SetActive(true);
-        botonMujer.SetActive(true);
-
-        RectTransform hombreTransform = hombreBTransform.GetComponent<RectTransform>();
-        RectTransform mujerTransform = mujerBTransform.GetComponent<RectTransform>();
-
-        hombreTransform.anchoredPosition = new Vector3(-78,-14,0);
-        mujerTransform.anchoredPosition = new Vector3(74,-14,0);
-    }
-
-    public void BotonYes()
+    public void introHistoria()
     {
         canvasJugador.SetActive(false);
         canvasHistoria.SetActive(true);
     }
 
-    public void BotonSiguinete1()
+    public void SeleccionarPais()
     {
-        panelHistoria1.SetActive(false);
-        panelHistoria2.SetActive(true);
+        presentacion.SetActive(false);
+        seleccionarPais.SetActive(true);
     }
 
-    public void BotonSiguinete2()
+    public void Brazil()
     {
-        panelHistoria2.SetActive(false);
-        panelHistoria3.SetActive(true);
+        seleccionarPais.SetActive(false);
+        paisBrazil.SetActive(true);
+    }
+    public void Argentina()
+    {
+        seleccionarPais.SetActive(false);
+        paisArgentina.SetActive(true);
+    }
+
+    public void ControlBrazil()
+    {
+        paisBrazil.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    public void ControlArgentina()
+    {
+        paisArgentina.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    public void IniciarJuego()
+    {
+        SceneManager.LoadScene("MapaBrasil");
     }
 }
